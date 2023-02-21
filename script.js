379e7cf9e2816af57e5c80cd5f5797f52ac839b2
Gaspare <Gas9220@users.noreply.github.com>
2023-02-21 14:49:14 +0100

    Aggiunto prompt per l'età

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ let kilometers = Number(prompt('Quanti km vuoi percorrere?'))
 // - Se il numero di chilometri è un numero vado avanti
 // - Altrimenti ripropongo il prompt
 
-if (typeof kilometers !== 'number')  {
-    kilometers = Number(prompt('Quanti km vuoi percorrere?'))
+
+
+// 2. Chiedo all'utente tramite un prompt la sua età e conservo il valore in una variabile
+let yourAge = Number(prompt('Quanti anni hai?'))
+
+//  - Mi assicuro che sia stato inserito un'età valida ( non negativa o superiore a 100 anni)
+//   - Se età inserita > 0 || età inserita < 100 vado avanti
+//   - Altrimenti ripropongo il prompt
+if (yourAge < 0 || yourAge > 100) {
+    yourAge = Number(prompt('Quanti anni hai?'))
 }
+
