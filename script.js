e8ea214d16435dd32a72a6ce52c259761a015707
Gaspare <Gas9220@users.noreply.github.com>
2023-02-22 09:21:15 +0100

    Correzioni nel file js

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
-// 1. Chiedo all'utente tramite un prompt il numero dei chilometri che vuole percorrere e conservo il valore in una variabile
-let kilometers = Number(prompt('Quanti km vuoi percorrere?'))
+'use strict';
 
-document.getElementById('kilometers').innerHTML = "Percorrerai " + kilometers + "km"
+// 1. Chiedo all'utente tramite un prompt il numero dei chilometri che vuole percorrere e conservo il valore in una variabile
+let kilometers = Number(prompt('Quanti km vuoi percorrere?'));
+
+document.getElementById('kilometers').innerHTML = "Percorrerai " + kilometers + "km";
 
 // - Mi assicuro che sia stato inserito un numero
 // - Se il numero di chilometri è un numero vado avanti
 // - Altrimenti ripropongo il prompt
 
 // 2. Chiedo all'utente tramite un prompt la sua età e conservo il valore in una variabile
-let yourAge = Number(prompt('Quanti anni hai?'))
+let yourAge = Number(prompt('Quanti anni hai?'));
 
 //  - Mi assicuro che sia stato inserito un'età valida ( non negativa o superiore a 100 anni)
 //   - Se età inserita > 0 || età inserita < 100 vado avanti
 //   - Altrimenti ripropongo il prompt
 
 if (yourAge < 0 || yourAge > 100) {
-    yourAge = Number(prompt('Quanti anni hai?'))
+    yourAge = Number(prompt('Quanti anni hai?'));
 }
 
-document.getElementById('age').innerHTML = "Hai un età di " + yourAge + " anni"
+document.getElementById('age').innerHTML = "Hai un età di " + yourAge + " anni";
 
 // 3. Calcolo il potenziale prezzo del biglietto, numero di chilometri * 0,21€ ( costo al km).
-const potentialPrice =  kilometers * 0.21
+const potentialPrice =  kilometers * 0.21;
 
-document.getElementById('potentialPrice').innerHTML = "Il prezzo originale è di " + potentialPrice.toFixed(2) + "€"
+document.getElementById('potentialPrice').innerHTML = "Il prezzo originale è di " + potentialPrice.toFixed(2) + "€";
 
 // 4. Calcolo il prezzo finale (formattato con massimo 2 decimali) il base alle regole stabilite
-let finalPrice = 0
+let finalPrice = 0;
 
 //  - Se eta inserita < 18 applico sconto del 20% con la seguente formula: prezzo finale = (prezzo potenziale * 20) / 100
 //  - Se eta inserita > 65 applico sconto del 40% con la seguente formula: prezzo finale = (prezzo potenziale * 40) / 100
 //  - Altrimenti il prezzo finale sarà uguale al prezzo potenziale
 
 if (yourAge < 18) {
-    const discountAmount = (potentialPrice * 20) / 100
-    finalPrice = potentialPrice - discountAmount
+    const discountAmount = (potentialPrice * 20) / 100;
+    finalPrice = potentialPrice - discountAmount;
 } else if (yourAge > 65) {
-    const discountAmount = (potentialPrice * 40) / 100
-    finalPrice = potentialPrice - discountAmount
+    const discountAmount = (potentialPrice * 40) / 100;
+    finalPrice = potentialPrice - discountAmount;
 } else {
-    finalPrice = potentialPrice
+    finalPrice = potentialPrice;
 }
 
-document.getElementById('finalPrice').innerHTML = "Il prezzo finale è di " + finalPrice.toFixed(2) + "€"
+document.getElementById('finalPrice').innerHTML = "Il prezzo finale è di " + finalPrice.toFixed(2) + "€";
 
 // Stampo in console il prezzo finale
-console.log(`In base ai ${kilometers}km che hai percorso e alla tua età ${yourAge} anni, il costo del biglietto è ${finalPrice.toFixed(2)}€`)
+console.log(`In base ai ${kilometers}km che hai percorso e alla tua età ${yourAge} anni, il costo del biglietto è ${finalPrice.toFixed(2)}€`);
--- script2.js
+++ script2.js
@@ -1,68 +1,70 @@
+'use strict';
+
 // 1. Chiedo all'utente tramite un prompt il numero dei chilometri che vuole percorrere e conservo il valore in una variabile
-let kilometers = Number(prompt('Quanti km vuoi percorrere?'))
+let kilometers = Number(prompt('Quanti km vuoi percorrere?'));
 
 // - Mi assicuro che sia stato inserito un numero
 // - Se il numero di chilometri è un numero vado avanti
 // - Altrimenti ripropongo il prompt
 
 while (isNaN(kilometers)) {
-    kilometers = Number(prompt('Quanti km vuoi percorrere?'))
+    kilometers = Number(prompt('Quanti km vuoi percorrere?'));
 }
 
-document.getElementById('kilometers').innerHTML = "Percorrerai " + kilometers + "km"
+document.getElementById('kilometers').innerHTML = "Percorrerai " + kilometers + "km";
 
 // // 2. Chiedo all'utente tramite un prompt la sua età e conservo il valore in una variabile
-let yourAge = prompt('Quanti anni hai?')
+let yourAge = prompt('Quanti anni hai?');
 
 //- Mi assicuro che sia stato inserito un'età valida ( non negativa o superiore a 100 anni)
 //- Se età inserita > 0 || età inserita < 100 vado avanti
 //- Altrimenti ripropongo il prompt
 
 while (isNaN(yourAge) || yourAge < 0 || yourAge > 100) {
-    yourAge = Number(prompt('Quanti anni hai?'))
+    yourAge = Number(prompt('Quanti anni hai?'));
 }
 
-document.getElementById('age').innerHTML = "Hai un età di " + yourAge + " anni"
+document.getElementById('age').innerHTML = "Hai un età di " + yourAge + " anni";
 
 // 3. Calcolo il potenziale prezzo del biglietto, numero di chilometri * 0,21€ ( costo al km).
-const potentialPrice = kilometers * 0.21
-document.getElementById('potentialPrice').innerHTML = "Il prezzo originale è di " + potentialPrice.toFixed(2) + "€"
+const potentialPrice = kilometers * 0.21;
+document.getElementById('potentialPrice').innerHTML = "Il prezzo originale è di " + potentialPrice.toFixed(2) + "€";
 
 // 4. Calcolo a quanto ammonta lo sconto
 //  - Se eta inserita < 18 applico sconto del 20% con la seguente formula: prezzo finale = (prezzo potenziale * 20) / 100
 //  - Se eta inserita > 65 applico sconto del 40% con la seguente formula: prezzo finale = (prezzo potenziale * 40) / 100
 //  - Altrimenti il prezzo finale sarà uguale al prezzo potenziale
 
-let message = ""
-let discountAmount = calculateDiscount(yourAge)
+let message = "";
+let discountAmount = calculateDiscount(yourAge);
 
 function calculateDiscount(age) {
-    const seniorDiscount = 40
-    const youngDiscount = 20
-    let discount = 0
+    const seniorDiscount = 40;
+    const youngDiscount = 20;
+    let discount = 0;
 
     if (age < 18) {
-        discount = (potentialPrice * youngDiscount) / 100
-        message = `Hai ricevuto ${discount.toFixed(2)}€ di sconto perchè sei minore di 18 anni`
+        discount = (potentialPrice * youngDiscount) / 100;
+        message = `Hai ricevuto ${discount.toFixed(2)}€ di sconto perchè sei minore di 18 anni`;
     } else if (age > 65) {
-        discount = (potentialPrice * seniorDiscount) / 100
-        message = `Hai ricevuto ${discount.toFixed(2)}€ di sconto perchè sei maggiore di 65 anni`
+        discount = (potentialPrice * seniorDiscount) / 100;
+        message = `Hai ricevuto ${discount.toFixed(2)}€ di sconto perchè sei maggiore di 65 anni`;
     } else {
-        discount = 0
-        message = `Non hai ricevuto alcuno sconto`
+        discount = 0;
+        message = `Non hai ricevuto alcuno sconto`;
     }
 
-    return discount
+    return discount;
 }
 
-let messageElement = document.getElementById('message')
-messageElement.innerHTML = message
-messageElement.classList.remove('d-none')
+let messageElement = document.getElementById('message');
+messageElement.innerHTML = message;
+messageElement.classList.remove('d-none');
 
 // Calcolo il prezzo finale
-let finalPrice = potentialPrice - discountAmount
-document.getElementById('finalPrice').innerHTML = "Il prezzo finale è di " + finalPrice.toFixed(2) + "€"
+let finalPrice = potentialPrice - discountAmount;
+document.getElementById('finalPrice').innerHTML = "Il prezzo finale è di " + finalPrice.toFixed(2) + "€";
 
 
 // // Stampo in console il prezzo finale
-console.log(`In base ai ${kilometers}km che hai percorso e alla tua età ${yourAge} anni, il costo del biglietto è ${finalPrice.toFixed(2)}€. Il costo iniziale era ${potentialPrice.toFixed(2)}. ${message}`)
+console.log(`In base ai ${kilometers}km che hai percorso e alla tua età ${yourAge} anni, il costo del biglietto è ${finalPrice.toFixed(2)}€. Il costo iniziale era ${potentialPrice.toFixed(2)}. ${message}`);
